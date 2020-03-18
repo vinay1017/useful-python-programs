392081588bf3f8ea900b4a17371d33f8cfd17244
Vinay <vinay1017@gmail.com>
2020-03-17 20:48:56 -0500

    Added some Leetcode practice problems answers.

---
+++ reversalOfInt.py
@@ -0,0 +1,35 @@
+class reversal(object):
+
+    def reverseInt(x):
+        """
+        Desc: A simple integer reversal program. Done for a practice problem on LeetCode. A number ending with 0
+        shouldn't be reversed to have the 0 at all in front and a number with a leading negative sign should not have
+        the negative sign at all if reversed.
+        """
+        if "0" in str(x):
+            y = str(x)[::-1]
+            y = y.replace("0", "")
+            y = "-" + y
+
+            if "-" in str(x):
+                y = y.replace("-", "")
+                y = "-" + y
+
+        elif "-" in str(x):
+            y = str(x)[::-1][0:5]
+            y = y.replace("-", "")
+            y = "-" + y
+        else:
+            y = str(x)[::-1]
+
+        return y
+
+    def reverseString(s):
+        ## work in progress
+        return s
+
+print(reversal.reverseInt(123))
+print(reversal.reverseInt(-123))
+print(reversal.reverseInt(-120))
+
+
